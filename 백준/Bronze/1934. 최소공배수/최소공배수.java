5b85bb738bdb7db3552cf8ff12588cdb00e3cc04
Josieon <76546008+josieon@users.noreply.github.com>
2023-02-07 17:12:48 +0900

    [Bronze I] Title: 최소공배수, Time: 180 ms, Memory: 14992 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -9,9 +9,11 @@ public class Main {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int A = Integer.parseInt(st.nextToken()), B = Integer.parseInt(st.nextToken());
             int gcd = gcd(A, B);
+            // 최소공배수 = 두 수의 곱 / 최대공약수
             System.out.println(A * B / gcd);
         }
     }
+    // 최대공약수
     public static int gcd(int A, int B){
         if(A == 0)
             return B;

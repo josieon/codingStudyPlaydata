b2046dcca630af2479a3cb75f2f20933891b1e6f
Josieon <76546008+josieon@users.noreply.github.com>
2023-02-10 11:47:25 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 184 ms, Memory: 15772 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/josieon_골드바흐의 추측.java
+++ 백준/Silver/9020. 골드바흐의 추측/josieon_골드바흐의 추측.java
@@ -4,11 +4,13 @@ import java.io.*;
 public class Main{
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());    // 테스트 케이스 수
         StringBuilder sb = new StringBuilder();
         while(T-- > 0){
             int n = Integer.parseInt(br.readLine());
+            // 입력된 짝수의 1/2부터 감소시키면서 탐색
             for(int i = n/2; i > 1; i--){
+                // i와 n-i 모두 소수인 경우 출력 및 반복문 중단
                 if(isPrime(i) && isPrime(n-i)){
                     sb.append(i).append(" ").append(n-i).append("\n");
                     break;
@@ -17,6 +19,7 @@ public class Main{
         }
         System.out.println(sb);
     }
+    // 소수 판별 함수
     public static boolean isPrime(int n){
         if(n == 1 || n == 0)
             return false;

f8386572aa7f6bc70b8d99c9bdf26dec263e2988
Josieon <76546008+josieon@users.noreply.github.com>
2023-02-10 12:07:53 +0900

    [Bronze V] Title: 영수증, Time: 128 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/josieon_영수증.java
+++ 백준/Bronze/25304. 영수증/josieon_영수증.java
@@ -3,13 +3,16 @@ import java.util.*;
 public class Main{
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // X의 범위가 1,000,000,000. 참고로, int의 범위는 2,147,483,647까지.
         long X = Long.parseLong(br.readLine());
         int N = Integer.parseInt(br.readLine());
         long total = 0;
+        // N개의 아이템 값 계산
         while(N-- > 0){
             StringTokenizer st = new StringTokenizer(br.readLine());
             total += Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken());
         }
+        // 비교해서 출력
         if(total == X)
             System.out.println("Yes");
         else

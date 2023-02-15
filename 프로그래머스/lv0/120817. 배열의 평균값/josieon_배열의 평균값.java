62eadac9d5212356ec328dac262b3a0200a2bc84
Josieon <76546008+josieon@users.noreply.github.com>
2023-02-15 12:28:15 +0900

    Delete 프로그래머스/lv0/120817. 배열의 평균값 directory

--- 프로그래머스/lv0/120817. 배열의 평균값/josieon_배열의 평균값.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public double solution(int[] numbers) {
-        double answer = 0;
-        for(int i : numbers)
-            answer += i;
-        return answer / numbers.length;
-    }
-}

9caddb9646b44f58c2bca7f75ba087299070a45a
jaynamm <48994100+jaynamm@users.noreply.github.com>
2023-02-17 15:40:48 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/jaynamm_콜라츠 추측.py
@@ -0,0 +1,19 @@
+def solution(num):
+    cnt = 0
+    
+    if num == 1:
+        return cnt
+    
+    while num != 1:
+        if cnt == 500:
+            cnt = -1
+            return cnt
+        
+        if num % 2 == 0:
+            num = num // 2
+            cnt += 1
+        else:
+            num = num * 3 + 1
+            cnt += 1
+    
+    return cnt

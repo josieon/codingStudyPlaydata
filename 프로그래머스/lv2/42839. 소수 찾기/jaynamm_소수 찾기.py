d2201a5034f0406fdfb4463b3e94a0cd4325c625
jaynamm <48994100+jaynamm@users.noreply.github.com>
2023-02-17 11:39:45 +0900

    [level 2] Title: 소수 찾기, Time: 0.55 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42839. 소수 찾기/jaynamm_소수 찾기.py
@@ -0,0 +1,24 @@
+from itertools import permutations
+
+def is_prime(n):
+    if n == 0 or n == 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+def solution(numbers):
+    answer = 0
+    nums = []
+    
+    for i in range(1, len(numbers)+1):
+        nums += list(set(permutations(numbers, i)))
+    
+    tmp = [int("".join(n)) for n in nums]
+    
+    for t in list(set(tmp)):
+        if is_prime(t):
+            answer += 1
+    
+    return answer

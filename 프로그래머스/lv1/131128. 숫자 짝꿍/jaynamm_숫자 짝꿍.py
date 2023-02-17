3be83cb8da5830d17c34fbddc9d1891bd96887b6
jaynamm <48994100+jaynamm@users.noreply.github.com>
2023-02-17 15:23:38 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/jaynamm_숫자 짝꿍.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+def solution(X, Y):
+    answer = ''
+    xc = [0] * 10
+    yc = [0] * 10
+    
+    for i in X:
+        xc[int(i)] += 1
+    for i in Y:
+        yc[int(i)] += 1
+    
+    for i in range(9,-1,-1):
+        if i == 0:
+            if xc[i] and yc[i] and not answer:
+                return "0"
+        answer += (str(i) * min(xc[i], yc[i]))
+        
+    if answer == "":
+        return "-1"
+    
+    return answer

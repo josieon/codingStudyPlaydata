7daf770638a903565971bc1c27cc75dd607282af
JAY <48994100+jaynamm@users.noreply.github.com>
2023-02-26 19:12:27 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/jaynamm_최댓값.py
+++ 백준/Bronze/2566. 최댓값/jaynamm_최댓값.py
@@ -1,10 +1,4 @@
-nums = [list(map(int, input().split())) for _ in range(9)]
-
-max_list = [0, 0, 0]
-
-for i in range(9):
-    for j in range(9):
-        if max_list[0] <= nums[i][j]:
-            max_list = [nums[i][j], i+1, j+1]
-
-print(max_list[0], max_list[1], max_list[2])
+nums = [*map(int,open(0).read().split())]
+mx = max(nums)
+mxid = nums.index(mx)
+print(mx, (mxid//9)+1, (mxid%9)+1)
